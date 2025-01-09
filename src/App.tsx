cf64c10277a584f5fe1598e844b949f06347eb20
Mohaanaaaa <mohanay52@gmail.com>
2025-01-09 13:44:53 +0530

    add on link

--- src/App.tsx
+++ src/App.tsx
@@ -95,7 +95,7 @@ function App() {
             <a href="https://github.com/Mohaanaaaa" target="_blank" rel="noopener noreferrer" className="p-2 rounded-full hover:bg-gray-700/20">
               <Github className="w-5 h-5" />
             </a>
-            <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer" className="p-2 rounded-full hover:bg-gray-700/20">
+            <a href="https://www.linkedin.com/in/mohana-y-2a121914b/" target="_blank" rel="noopener noreferrer" className="p-2 rounded-full hover:bg-gray-700/20">
               <Linkedin className="w-5 h-5" />
             </a>
             <a href="mailto: mohanay52@gmail.com" className="p-2 rounded-full hover:bg-gray-700/20">
